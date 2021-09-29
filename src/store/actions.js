27d815f67ca8d580a630a039be825ed23b906187
elmahdim <elmahdi.mahmoud@live.com>
2021-09-29 20:19:29 +0300

    Add column key to tasks/add action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,14 @@
 import { TASKS_ADD } from "./actionsTypes";
 import { getUniqueID } from "../utils";
 
-export function tasksAdd({ title, assignee }) {
+export function tasksAdd({ title, assignee, column = "todo" }) {
   return {
     type: TASKS_ADD,
     payload: {
       id: getUniqueID(),
       title,
       assignee,
+      column,
     },
   };
 }
