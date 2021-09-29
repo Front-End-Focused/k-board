77dfd7339c90075af581cf5dbfb6c952f5be857a
elmahdim <elmahdi.mahmoud@live.com>
2021-09-29 19:43:18 +0300

    Add redux, react-redux. Wrap App in redux Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./App";
 
+const rootReducer = () => ({});
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
